3eca8dafb803d77afafe210ed36ecab328baa08d
Danh <43163751+danhpham1@users.noreply.github.com>
2020-12-03 10:54:35 +0700

    fix Env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,8 +5,8 @@
 export const environment = {
   production: false,
 
-  // EndPointAPI: 'https://radiant-stream-23882.herokuapp.com',
-  EndPointAPI: 'http://localhost:3000',
+  EndPointAPI: 'https://radiant-stream-23882.herokuapp.com',
+  // EndPointAPI: 'http://localhost:3000',
 
   APIPrefix: '/api',
   APIVersion: '/v1',
